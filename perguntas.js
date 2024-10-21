bad99f4de603ed225f49a9b0fe4693b885861161
analuvvitoria21 <165676097+analuvvitoria21@users.noreply.github.com>
2024-10-21 12:02:44 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,17 +1,23 @@
 criaCartao(
     'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Quantos ano um cachorro vive?',
+    '`15 anos`'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual a capital da Espanha?',
+    'A capital da Espanha é Madrid'
 )
 
 criaCartao(
     'Programação',
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz com licença em inglês?',
+    'Excuse me'
 )
